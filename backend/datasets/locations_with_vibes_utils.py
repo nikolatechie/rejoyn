63f052dbf1d28db98a20d127502bf1348aa2fb00
nikolatechie <nikola-grujic@hotmail.com>
2025-05-03 10:58:49 +0200

    Add more features

--- backend/datasets/locations_with_vibes_utils.py
+++ backend/datasets/locations_with_vibes_utils.py
@@ -1,4 +1,18 @@
+import json
+from typing import List
 import pandas as pd
+
+# List of features related to trip preferences
+FEATURES: None | List[str] = [
+    "art_and_culture",
+    "beach",
+    "outdoor_adventures",
+    "nightlife_and_entertainment",
+    "great_food",
+    "underrated_destinations",
+    "safety",  # Added
+    "weather",  # Added
+]
 
 
 def load_csv_values(file_path: str) -> pd.DataFrame:
@@ -20,7 +34,10 @@ def load_csv_values(file_path: str) -> pd.DataFrame:
         return None
 
 
-def extract_unique_vibes(file_path: str) -> None | list[str]:
+def _extract_unique_vibes(file_path: str) -> None | list[str]:
+    if FEATURES is not None:
+        return FEATURES
+
     df = load_csv_values(file_path)
     if df is not None and "vibes" in df.columns:
         unique_vibes = set()
@@ -31,13 +48,18 @@ def extract_unique_vibes(file_path: str) -> None | list[str]:
             except Exception as e:
                 print(f"Error processing row value: {e}")
         # unique_vibes = list(unique_vibes)
-        return list(
-            unique_vibes
-        )  # ['art_and_culture', 'great_food', 'nightlife_and_entertainment', 'outdoor_adventures', 'beach', 'underrated_destinations']
+        return list(unique_vibes)
     else:
         print("Column 'vibes' not found in the DataFrame or DataFrame is None.")
         return None
 
 
-unique_vibes = extract_unique_vibes("iata_airports_and_locations_with_vibes.csv")
+def _parse_vibes(vibes):
+    if pd.isna(vibes) or vibes == "null":
+        pass
+
+    return json.loads(vibes)
+
+
+unique_vibes = _extract_unique_vibes("iata_airports_and_locations_with_vibes.csv")
 print(unique_vibes)
