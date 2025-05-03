b062459e20f39eca1c8d975e442214a14defadc3
nikolatechie <nikola-grujic@hotmail.com>
2025-05-03 18:33:12 +0200

    Register page as default

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,7 +1,7 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Register from "./register";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
@@ -9,5 +9,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Register />
 }
