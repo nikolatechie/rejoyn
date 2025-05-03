ec764c80eef9b58e2d26dd753511a0f605b9c093
nikolatechie <nikola-grujic@hotmail.com>
2025-05-03 14:03:18 +0200

    Add database layer

--- backend/main.sql
+++ backend/main.sql
@@ -2,6 +2,8 @@ create database rejoyn;
 
 use rejoyn;
 
+show tables;
+
 create table users (
 	id int not null primary key auto_increment,
     full_name varchar(150) not null,
@@ -12,3 +14,30 @@ create table users (
 );
 
 -- select * from users
+
+create table destinations_features (
+	id int not null primary key auto_increment,
+    feature varchar(100) not null unique
+);
+
+INSERT INTO destinations_features (feature)
+VALUES 
+    ('art_and_culture'),
+    ('beach'),
+    ('outdoor_adventures'),
+    ('nightlife_and_entertainment'),
+    ('great_food'),
+    ('underrated_destinations'),
+    ('safety'),
+    ('weather');
+
+select * from destinations_features;
+
+create table users_preferences (
+	id int not null primary key auto_increment,
+    user_id int not null,
+    feature_id int not null,
+    score int not null,
+    foreign key (user_id) references users(id),
+    foreign key (feature_id) references destinations_features(id)
+);
