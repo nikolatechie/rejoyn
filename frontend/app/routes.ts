88e4140cced40a58a5012a73aced9625601aae10
yowix <slavkojovich@gmail.com>
2025-05-03 15:33:28 +0200

    Add login and register pages to router

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,3 +1,13 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),  // This is your home route
+  {
+    path: '/register',
+    file: 'routes/register.tsx',
+  },
+  {
+    path: '/login',
+    file: 'routes/login.tsx',
+  },
+] satisfies RouteConfig;
---
+++ frontend/app/routes/register.tsx
@@ -0,0 +1,4 @@
+
+export default function Register() {
+  return <h2>Register here</h2>
+}
